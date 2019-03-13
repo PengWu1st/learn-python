0bc64421d35221493dbd0b5e4f21ce3683158357
PengWu1st <wupeng1127@foxmail.com>
2019-03-13 14:02:05 +0800

    quick start

--- flask/hello.py
+++ flask/hello.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'Index Page!'
 
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % subpath
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return 'do the login'
+    else:
+        return 'show the login form'
+        
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
